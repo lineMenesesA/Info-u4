67ac91ea7afb2bf0a987b60f38592465c5ed0a4e
lineUbuntu <l17181089@slp.tecnm.mx>
2021-11-18 13:22:47 -0600

    Cambio de nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,9 @@
                         </g>
                     </svg>
                 </div>
+                <div>
+                <div id="lab_nom" align="left" style="color:#ff006c;"> &nbsp;JACQUELINE Meneses Arriaga</div>
+                </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
